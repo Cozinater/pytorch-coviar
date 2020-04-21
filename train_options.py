0fa1d785182f1207c7ca2f56541a000ac5066d5a
Cozinater <ray.tan180@gmail.com>
2020-04-22 01:48:03 +0800

    added new option to choose feature branch

--- train_options.py
+++ train_options.py
@@ -23,10 +23,10 @@ parser.add_argument('--num_segments', type=int, default=3,
                     help='number of TSN segments.')
 parser.add_argument('--no-accumulation', action='store_true',
                     help='disable accumulation of motion vectors and residuals.')
-parser.add_argument('--no-TopKAtt', type=bool, default=False,
-                    help='enable TopKAtt feature in the model')
-parser.add_argument('--topk', type=int, default=16,
-                    help='enable TopKAtt feature in the model')
+parser.add_argument('--feature-branch', type=str, default=None, choices=['topKAtt',' basenl', 'a2block'],
+                    help='feature branch used between layer 3 and 4 of resnet')
+parser.add_argument('--topk', type=int, default=None,
+                    help='topk parameter for TopKAtt()')
 
 # Training.
 parser.add_argument('--epochs', default=500, type=int,
