1d9f47c305588de139de14d8dbca5e3f5372dab1
Cozinater <ray.tan180@gmail.com>
2020-04-22 02:07:57 +0800

    added a feature to combine mv and res

--- combine.py
+++ combine.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def main():
     parser = argparse.ArgumentParser(description="combine predictions")
-    parser.add_argument('--iframe', type=str, required=True,
+    parser.add_argument('--iframe', type=str,
                         help='iframe score file.')
     parser.add_argument('--mv', type=str, required=True,
                         help='motion vector score file.')
@@ -18,27 +18,47 @@ def main():
                         help='motion vector weight.')
     parser.add_argument('--wr', type=float, default=1.0,
                         help='residual weight.')
+                        
+    parser.add_argument('--noiframe', type=bool,
+    help='only the score of mv and residual is combined. iframe is not included')
 
     args = parser.parse_args()
-
-    with np.load(args.iframe) as iframe:
-        with np.load(args.mv) as mv:
-            with np.load(args.res) as residual:
+    
+    if (args.noiframe):
+        with np.load(args.mv, allow_pickle=True) as mv:
+            with np.load(args.res, allow_pickle=True) as residual:
                 n = len(mv['names'])
 
-                i_score = np.array([score[0][0] for score in iframe['scores']])
                 mv_score = np.array([score[0][0] for score in mv['scores']])
                 res_score = np.array([score[0][0] for score in residual['scores']])
 
-                i_label = np.array([score[1] for score in iframe['scores']])
                 mv_label = np.array([score[1] for score in mv['scores']])
                 res_label = np.array([score[1] for score in residual['scores']])
-                assert np.alltrue(i_label == mv_label) and np.alltrue(i_label == res_label)
+                assert np.alltrue(res_label == mv_label)
+                combined_score =  mv_score * args.wm + res_score * args.wr
 
-                combined_score = i_score * args.wi + mv_score * args.wm + res_score * args.wr
-
-                accuracy = float(sum(np.argmax(combined_score, axis=1) == i_label)) / n
+                accuracy = float(sum(np.argmax(combined_score, axis=1) == mv_label)) / n
                 print('Accuracy: %f (%d).' % (accuracy, n))
+
+    else:
+        with np.load(args.iframe,allow_pickle=True) as iframe:
+            with np.load(args.mv, allow_pickle=True) as mv:
+                with np.load(args.res, allow_pickle=True) as residual:
+                    n = len(mv['names'])
+
+                    i_score = np.array([score[0][0] for score in iframe['scores']])
+                    mv_score = np.array([score[0][0] for score in mv['scores']])
+                    res_score = np.array([score[0][0] for score in residual['scores']])
+
+                    i_label = np.array([score[1] for score in iframe['scores']])
+                    mv_label = np.array([score[1] for score in mv['scores']])
+                    res_label = np.array([score[1] for score in residual['scores']])
+                    assert np.alltrue(i_label == mv_label) and np.alltrue(i_label == res_label)
+
+                    combined_score = i_score * args.wi + mv_score * args.wm + res_score * args.wr
+
+                    accuracy = float(sum(np.argmax(combined_score, axis=1) == i_label)) / n
+                    print('Accuracy: %f (%d).' % (accuracy, n))
 
 if __name__ == '__main__':
     main()
